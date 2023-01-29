8a53694b171c3af58e0069bcd31a2292c51d6364
Anatoliy <bugrov.anatoliy@yandex.ru>
2023-01-29 23:37:26 +1000

    добавлены тесты

--- src/test/java/ru/practicum/shareit/item/dto/CommentRequestDtoTest.java
+++ src/test/java/ru/practicum/shareit/item/dto/CommentRequestDtoTest.java
@@ -7,6 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CommentRequestDtoTest {
     private CommentRequestDto commentRequestDto;
+
     @BeforeEach
     void setUp() {
         commentRequestDto = new CommentRequestDto();
